4458ae5548aade9df76c8f54d5bdc80d0ec19daf
mdnazril <96862439+mdnazril@users.noreply.github.com>
2022-06-07 14:12:10 +0800

    head tag

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,13 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>To-Do List</title>
+      <meta name="description" content="Simple todo website"></meta>
+      <meta name="author" content="mdnazril"></meta>
+    </Head>
     <GetTask />
+    </>
   )
 }
